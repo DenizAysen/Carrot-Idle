dd25a5e3b5ddb419ce7993317118744f5e1783e2
Turgut Deniz Aysen <45630354+DenizAysen@users.noreply.github.com>
2023-07-29 17:01:47 +0300

    Touch Class added

--- Carrot Clicker/Assets/Scripts/InputManager.cs
+++ Carrot Clicker/Assets/Scripts/InputManager.cs
@@ -15,12 +15,40 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-            ThrowRaycast();
+        if (Input.touchCount > 0)
+        {
+            ManageTouches();
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+                ThrowRaycast();
+        }        
+    }
+    private void ManageTouches()
+    {
+        Touch touch;
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            touch = Input.GetTouch(i);
+
+            if (touch.phase == TouchPhase.Began)
+                ThrowRaycastInMobile(touch.position);
+        }
     }
     private void ThrowRaycast()
     {
         RaycastHit2D hit =  Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+
+        if (hit.collider == null)
+            return;
+
+        onCarrotClicked?.Invoke();
+        onCarrotClickedPosition?.Invoke(hit.point);
+    }
+    private void ThrowRaycastInMobile(Vector2 touchPosition)
+    {
+        RaycastHit2D hit = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(touchPosition));
 
         if (hit.collider == null)
             return;
